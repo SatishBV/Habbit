22a5cd214382cb0aa36994a974741eed63f2eb94
Satish Bandaru <satish.bandaru@walmartlabs.com>
2020-05-11 12:23:54 +0530

    clean ignored files

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/s1b012r/Workspace/Training/Flutter workspace/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/s1b012r/Workspace/Training/Habbit"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/s1b012r/Workspace/Training/Flutter workspace/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
